00133488105859705c885e2cefe5cd17fb3ccf84
ejramirezdev <ejramirezdev@gmail.com>
2023-01-04 16:12:53 -0500

    primero

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # odin-recipes
+This is a repo to create a basic web design, understand and make use of the github basics.
---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Obtener el botón
+var button = document.querySelector("#cambiar-color");
+
+// Añadir un evento al botón
+button.addEventListener("click", function() {
+  // Cambiar el color de fondo
+  document.body.style.backgroundColor = "green";
+});
